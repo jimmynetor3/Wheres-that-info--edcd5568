ccb4d8c74a116200857b60431c5ce0e3674476fe
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2020-01-17 11:41:38 +0100

    Uitwerking van de exercise

---
+++ queries.sql
@@ -0,0 +1,4 @@
+SELECT * FROM series WHERE has_won_awards = true;
+SELECT * FROM series WHERE rating > 2.5 true;
+SELECT * FROM series WHERE country = NL AND language = NL;
+SELECT * FROM series WHERE seasons < 5;
